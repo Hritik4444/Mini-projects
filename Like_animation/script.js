2875d22b1276b56d17e63b34b7bd09eb646e191f
Hritik <hritiknegi9654@gmail.com>
2025-11-25 23:32:04 +0530

    final

--- Like_animation/script.js
+++ Like_animation/script.js
@@ -5,5 +5,13 @@ let i = document.querySelector('i')
 card.addEventListener('dblclick',function(){
     console.log("hello")
     i.style.opacity=1
+     setTimeout(function(){
+     i.style.transform= 'translate(-50%,-220%) rotate(60deg)'
+     },800)
+
+     setTimeout(function(){
+        i.style.opacity=0
+       i.style.transform='translate(-50%,-50%) rotate(0)'
+     },1200)
 
 })
