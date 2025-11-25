f800f74557543517095a9ed56a0642e7d0f666f6
Hritik <hritiknegi9654@gmail.com>
2025-11-25 23:06:47 +0530

    new project

---
+++ Like_animation/script.js
@@ -0,0 +1,9 @@
+let card= document.querySelector('#card')
+let box= document.querySelector('#box')
+let i = document.querySelector('i')
+
+card.addEventListener('dblclick',function(){
+    console.log("hello")
+    i.style.opacity=1
+
+})
